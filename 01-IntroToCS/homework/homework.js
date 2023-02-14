981b878662de0cc2701400a36c8db430e0e2e55d
SamuelMorado123 <gabrielsamuel2003b@gmail.com>
2023-02-14 07:02:50 -0500

    Intro MT

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,10 +1,33 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+   let j=0;
+   let m=0;
+   let i=0;
+      for (let index = num.toString().length; index > 0; index--) { 
+                
+                m= parseInt(num.toString()[index-1]);
+                j+=m*2**i
+                i++;             
+      } 
+      return j;
+}
 
-function DecimalABinario(num) {}
+function DecimalABinario(num) {
+   let k=num;
+   let i =0;
+   let res=100;
+  for (let index = 0; index < 100; index++) {
+    if(res==1){
+      k=k/2;
+      res=k;
+    }
+  }
+
+}
 
 module.exports = {
    BinarioADecimal,
    DecimalABinario,
 };
+
